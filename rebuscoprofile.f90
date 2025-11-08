fe0bb9cc5b2e4aad55b2bca9131f49ead4f59bf4
annamiche <annamichelini25102002@gmail.com>
2025-11-08 13:13:17 +0100

    Update rebusco.f90
    aggiunto Te_keV e temp_rebusco

--- rebuscoprofile.f90
+++ rebuscoprofile.f90
@@ -1,8 +1,8 @@
 parameter(jmax=5000)
-real*8, dimension(jmax) :: rr(jmax), rho_rebusco, temp_rebusco
-real*8 ::rmin,rmax
-real*8, dimension(jmax) :: r
-real*8 :: cmkpc, ne_rebusco
+
+real*8, dimension(jmax) :: rr, rho_rebusco, temp_rebusco, Te_keV, r
+real*8 :: rmin, rmax, cmkpc, ne_rebusco
+
 
 integer :: j
 
@@ -21,29 +21,29 @@ do j=1,jmax-1
 enddo
 rr(jmax)=rr(jmax-1)+(rr(jmax-1)-rr(jmax-2))
 !salva griglia
-open(10,file='grid_rebusco.dat',status='unknown')
+open(10,file='grid_reb.dat',status='unknown')
 do j=1,jmax
    write(10,*)real(r(j)/cmkpc),real(rr(j)/cmkpc)
 enddo
 close(10)
 
 ! profilo Rebusco
-open(20,file='density_rebusco.dat',status='unknown')
+open(20,file='density_reb.dat',status='unknown')
 do j=1,jmax
    rkpc = rr(j)/cmkpc
    ! Densità elettronica ne
    ne_rebusco = 4.6d-2 / (1.d0 + (rkpc/57.d0)**2.d0)**1.8d0 + 4.8d-3 / (1.d0 + (rkpc/200.d0)**2.d0)**0.87d0
    rho_rebusco(j) = 1.937d-24 * ne_rebusco  ! in g/cm^3
-   ! Temperatura Te(r) from keV to Kelvin
-   Te_keV = 7.d0 * (1.d0 + (rkpc/71.d0)**3) / (1.d0 + (rkpc/300.d0)**3)
-temp_rebusco(j) = Te_keV * 1.16d7
-!temp_rebusco(j) = 7.d0 * (1.d0 + (rkpc/71.d0)**3) / (2.d3 + (rkpc/71.d0)**3) * 1.16d7  ! in Kelvin perchè c'è * 1.16d7
+   ! Temperatura in keV 
+   Te_keV(j) = 7.d0 * (1.d0 + (rkpc/71.d0)**3) / (2.3d0 + (rkpc/71.d0)**3) !errore commesso prima era nello scrivere 2.3 su fortran si scrive 2.3d0 
+   !Temp in Kelvin
+   temp_rebusco(j) = Te_keV(j) * 1.16d7 
   
-   write(20,1000) rr(j)/cmkpc, rho_rebusco(j), temp_rebusco(j)
+   write(20,1000) rr(j)/cmkpc, rho_rebusco(j), Te_keV(j), temp_rebusco(j)
 enddo
 close(20)
 
-1000 format(3(1pe13.5))
+1000 format(4((1pe12.4)))
 end
 
 
