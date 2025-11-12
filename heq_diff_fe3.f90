cb9730979443c77a0ee6370833aa3fc9a4a24415
claudiapecorari29 <claudia.pecorari29@gmail.com>
2025-11-12 10:44:02 +0100

    Aggiunto commenti

--- heq_diff_fe3.f90
+++ heq_diff_fe3.f90
@@ -11,12 +11,12 @@ real*8, dimension(jmax) :: r,rr,vol,mnfw,&
         grvnfw,lnd, mdm_analytic, T, lnT,Mgas,&
         ne_reb, rho_reb, T_reb, zfe_obs,& !!!!Variables for diffusion program
         zfe_ex, zfe_s, rho_fe_ex, rhofe_s, z_fe, rho_fe, gradz_fe,rho_fe_obs
-   
+
 
 !!!!Declaration of real numerical values!!!!
-real*8 :: msol,mu,mp,rmin,rmax,mvir,rvir,mbgc,ahern,fb,rkpc,zfesol,zfe_ground,rho_jp1,rho_j, dt, D, t_final, time, n_cycle, v_t, l_t
+real*8 :: msol,mu,mp,rmin,rmax,mvir,rvir,mbgc,ahern,fb,rkpc,zfesol,zfe_ground,&
+          rho_jp1,rho_j, dt, D, t_final, time, n_cycle, v_t, l_t
 !constants
-
 msol = 1.989d33
 cmkpc = 3.084e21
 mu=0.62
@@ -26,7 +26,6 @@ mp=1.67265e-24
 years=3.156d7
 
 !set the grid
-
 rmin = 0.*cmkpc
 rmax = 3000.*cmkpc
 !Original domain!
@@ -36,29 +35,26 @@ enddo
 !Shifted domain!
 do j=1,jmax-1
    rr(j)=r(j)+0.5*(r(j+1)-r(j))     !!r_j+1/2
-enddo
+end do
 rr(jmax)=rr(jmax-1)+(rr(jmax-1)-rr(jmax-2))
 
 open(10,file='grid.dat',status='unknown')
 do j=1,jmax
    write(10,*)real(r(j)/cmkpc),real(rr(j)/cmkpc)
-enddo
+end do
 close(10)
 
 !!volumes of centered shells on the shifted domain!!
-
 vol(1)=4.1888*r(1)**3
 do j=2,jmax
    vol(j)=4.1888*(r(j)**3-r(j-1)**3)   
-enddo
+end do
 
 !  parametri del problema
-
 rho0nfw=7.35d-26
 rs=435.7*cmkpc
 rho0=8d-26 		      !!central density/initial condition to be modified
 ticm=8.9d7		      !!temperature of the ICM
-
 rvir=2797.*cmkpc
 r500=rvir/2.0d0
 fc=1.138799
@@ -71,9 +67,9 @@ ahern=10.*cmkpc/(1.+2.**(0.5))
 do j=1,jmax
    x=rr(j)/rs
    rhonfw(j)=rho0nfw/(x*(1.+x)**2)
-enddo
+end do
 
-open(20,file='masse_gradT.dat')
+open(20,file='masse_diff.dat')
 mnfw(1)=0.		!!initial dark matter mass
 mdark(1)=0.
 mhern(1)=0.
@@ -86,34 +82,33 @@ do j=2,jmax 		!!NB: starts at j=2 not j=1
    mdm_analytic(j)=4*3.14*rho0nfw*(rs**3)*(log(1.+x)-(r(j)/(r(j)+rs)))     !!DM analytical
    mdark(j)=mvir*(log(1.+x)-x/(1.+x))/fc				                        !!DM analytic NFW formula
    mhern(j)=mbgc*r(j)**2/(r(j)+ahern)**2				                        !!analytic stellar mass
-enddo
+end do
 !!Cycle on the Original domain!!
 do j=1, jmax
    write(20,1001)r(j)/cmkpc,mnfw(j)/msol,mdark(j)/msol,mhern(j)/msol, mdm_analytic(j)/msol
-enddo
+end do
 1001 format(5(1pe12.4))
 close(20)
 
 !! Temperature profile from a given analytical function !!
-open(20, file='temperature.dat')
+open(20, file='temperature_diff.dat')
 do j=1, jmax
     y=rr(j)/r500
     T(j)=ticm*1.35d0*((y/0.045d0)**1.9+0.45d0)/((y/0.045d0)**1.9+1)*(1+(y/0.6d0)**2)**(-0.45)
     lnT(j)=log(T(j))
     write(20,1003)rr(j)/cmkpc, T(j), ticm
-enddo
+end do
 1003 format(3(1pe12.4))
 close(20)
 
 !!gravitational energy
-
-open(20,file='grv_gradT.dat')
+open(20,file='grv_diff.dat')
 grvnfw(1)=0.          !! ok per alone NFW, isotermo o beta-model
 !!!!Cycle on the Original domain!!
 do j=2,jmax
    grvnfw(j)=guniv*(mnfw(j)+mhern(j))/r(j)**2		!!already takes into account the mass of the central galaxy BCG
    write(20,1002)r(j)/cmkpc,grvnfw(j)/msol
-enddo
+end do
 1002 format(2(1pe12.4))
 close(20)
 
@@ -123,17 +118,17 @@ lnd(1)=log(rho0)          !! mette il gas in eq. con il potenziale
 do j=2,jmax
    gg=grvnfw(j)
    lnd(j)=lnd(j-1)-(gg*(mu*mp)*(rr(j)-rr(j-1))/(boltz*T(j))+ lnT(j)-lnT(j-1))
-enddo
+end do
 !!Just passing from lnd(j) to rho(j)!!
 do j=1,jmax
    rho(j)=exp(lnd(j))  
-enddo
+end do
 
 !!Cycle on the Shifted domain!!
 Mgas(1)=0.
 do j=2,jmax
    Mgas(j)=Mgas(j-1)+rho(j-1)*vol(j)
-enddo
+end do
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!NOW THE REAL FE DIFFUSION PART STARTS!!!!!!!!!!!!!!!!!!!!!!!!!!!! ************************************************************************************************+
 !Comparison between our profiles, of density and temperature, and the Rebusco profiles!
  open(10,file='comparison.dat')
@@ -154,7 +149,7 @@ enddo
   rho_reb(j) = 1.937d-27 * ne_reb(j)
   T_reb(j) = 7 * ((1+(rkpc/71)**3)/(2.3+(rkpc/71)**3)) * KeV
   write(10,1000)rkpc,rho(j), rho_reb(j), T_reb(j)
- enddo 
+ end do 
  1000 format(4(1pe12.4))
 
 !!!!!!!!!!!Saving the initial values!!!!!!!!!!!!
