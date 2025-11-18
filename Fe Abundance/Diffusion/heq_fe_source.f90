0366efeb47332e1e2a6284214950f4a8cbd448db
claudiapecorari29 <claudia.pecorari29@gmail.com>
2025-11-18 23:59:49 +0100

    Update heq_fe_source.f90
    Aggiunto commenti

--- Fe Abundance/Diffusion/heq_fe_source.f90
+++ Fe Abundance/Diffusion/heq_fe_source.f90
@@ -1,6 +1,6 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!! Nuova versione parzialmente corretta: ciclo su n invece che su time, stampa 
-!! su file ma viene strano
+!! Codice per il caso solo sorgente, con l'opzione di scegliere da terminale se 
+!! avere i coefficienti stellare e delle supernovae costanti o variabili nel tempo
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 parameter(jmax=5000)
@@ -27,9 +27,10 @@ boltz=1.38066e-16
 guniv=6.6720e-8
 mp=1.67265e-24
 years=3.156d7
+KeV = 1.16d7
 t_now=13.7d9*years
 
-!  parametri del problema
+!parametri del problema
 rho0nfw=7.35d-26
 rs=435.7*cmkpc
 rho0=8d-26 		      !!central density/initial condition
@@ -40,6 +41,13 @@ fc=1.138799
 mvir=1.3e15*msol
 mbgc=1.d12*msol
 ahern=10.*cmkpc/(1.+2.**(0.5))
+zfesol= 1.8d-3
+zfe_ground = 0.4 * zfesol
+zfe_Ia=0.744/1.4
+v_t = 200d6
+l_t = 20 * cmkpc
+!D = 0.333333333* v_t * l_t
+D = 0.11 * 260d5 * 15 * cmkpc
 
 !set the grid
 rmin = 0.*cmkpc
@@ -55,7 +63,6 @@ enddo
 rr(jmax)=rr(jmax-1)+(rr(jmax-1)-rr(jmax-2))
 
 !!volumes of centered shells on the shifted domain!!
-
 vol(1)=4.1888*r(1)**3
 do j=2,jmax
    vol(j)=4.1888*(r(j)**3-r(j-1)**3)   
@@ -74,7 +81,7 @@ mhern(1)=0.
 mdm_analytic(1)=0.
 mdm_analytic(1)=0.
 !!Cycle on the Original domain!!
-do j=2,jmax 		!!NB: starts at j=2 not j=1
+do j=2,jmax 
    x=r(j)/rs
    mnfw(j)=mnfw(j-1)+rhonfw(j-1)*vol(j)					                     !!DM mass discrete NFW formula in pdf centered in j-1/2
    mdm_analytic(j)=4*3.14*rho0nfw*(rs**3)*(log(1.+x)-(r(j)/(r(j)+rs)))     !!DM analytical
@@ -97,13 +104,12 @@ do j=2,jmax
 enddo
 
 !calculate the gas density
-lnd(1)=log(rho0)          !! mette il gas in eq. con il potenziale
+lnd(1)=log(rho0)      
 !!Cycle on the Shifted domain for the density and the original domain for the temperature!!
 do j=2,jmax
    gg=grvnfw(j)
    lnd(j)=lnd(j-1)-(gg*(mu*mp)*(rr(j)-rr(j-1))/(boltz*T(j))+ lnT(j)-lnT(j-1))
 enddo
-!!Just passing from lnd(j) to rho(j)!!
 do j=1,jmax
    rho(j)=exp(lnd(j))  
 enddo
@@ -114,55 +120,30 @@ do j=2,jmax
    Mgas(j)=Mgas(j-1)+rho(j-1)*vol(j)
 enddo
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!NOW THE REAL FE DIFFUSION PART STARTS!!!!!!!!!!!!!!!!!!!!!!!!!!!! ************************************************************************************************+
-!Comparison between our profiles, of density and temperature, and the Rebusco profiles!
-  !!In this cycle we are calculating the gas density and the temperature profile used by Rebusco, from XMM Newton data!!
-  zfesol= 1.8d-3
-  zfe_ground = 0.4 * zfesol
-  zfe_Ia=0.744/1.4
-  v_t = 200d6
-  l_t = 20 * cmkpc
-  KeV = 1.16d7
-  !D = 0.333333333* v_t * l_t
-  D = 0.11 * 260d5 * 15 * cmkpc
-
-!!!!!!!!!!!Saving the initial values!!!!!!!!!!!!
-open(20, file='initial_fe_source.dat' )
-
-!!!!!!!!!!!Saving the final values!!!!!!!!
+!!!!!!!!!!!Files for the final values!!!!!!!!
 open(30, file='final_fe_source_coeff.dat')   !!file for the variable coefficients case
 open(40, file= 'final_fe_source.dat')        !!file for the constant coefficients case
 !open(50, file= 'Mass_fe_source.dat')
 
 do j = 1, jmax
  !Rescaled radius!
-    x=rr(j)/(80.*cmkpc)
- !We are creating the initial condition data file!
+ x=rr(j)/(80.*cmkpc)
 
- !DIFFUSION CASE!
  !!Computing the Fe abundance profile in Perseus Cluster ! 
-  !!Perseus: here I would remove the factor 1.15/1.15, because for definition is equal to 1!!
-   zfe_obs(j) = zfesol * 0.3 * 1.4 * 1.15 * (2.2 + x**3)/(1 + x**3)/1.15  
+ zfe_obs(j) = zfesol * 0.3 * 1.4 * 1.15 * (2.2 + x**3)/(1 + x**3)/1.15  
 
-   !!!Cleaning operation from the abundance_fe di background!
-    !!!!zfe_ex is reffering to data of the Fe abundance without the background contribution!!  
-   zfe_ex(j) = zfe_obs(j) - zfe_ground 
-   zfe_ex(j)= max(zfe_ex(j),0.) !calcola il max-->per successivo controllo! Forse lo fa perchè la differenza potrebbe andare in negativo 
-                                !quindi sostituisce l'eccesso negativo con lo 0 per scanso di equivoci
-    !!!!zfe_ex will be the initial condition for the diffusion case
- !!Computing the Fe density: t = 0!!
-   
-  !!!From raw data!!!
-   rho_fe_obs(j) = rho(j) * zfe_obs(j)/1.4
-
-  !!!From cleaned data!!!
-   rho_fe_ex(j) = rho(j) * zfe_ex(j)/1.4
+ !!!Cleaning operation from the abundance_fe di background
+ zfe_ex(j) = zfe_obs(j) - zfe_ground   !!!!zfe_ex is reffering to data of the Fe abundance without the background contribution!!  
+ zfe_ex(j)= max(zfe_ex(j),0.)
+ !!!!zfe_ex will be the initial condition for the diffusion case
  
- write(20,1004) rr(j)/cmkpc, zfe_obs(j), zfe_ex(j)
-enddo
-1004 format(4(1pe12.4))
+ !!Computing the Fe density: t = 0!!
+ !!!From raw data!!!
+ rho_fe_obs(j) = rho(j) * zfe_obs(j)/1.4
+ !!!From cleaned data!!!
+ rho_fe_ex(j) = rho(j) * zfe_ex(j)/1.4
+end do
 
-Print*, zfe_obs(1)
-Print*, zfe_obs(100)
 !!!!!!!!!!!!!!!!!!Computing the initial mass of the iron profile
 M0_fe(1) = 0.
 do j=2,jmax
@@ -181,10 +162,9 @@ read(*, *) time                     !!ex. to see the evolution in 5 Gyr, initial
 time = time* years
 dt = 0.4 * (r(5) - r(4))**2 / (2. * D)
 n_cycle=(t_now-time)/dt
-Print*, "Intervallo temporale ", dt
+!Print*, "Intervallo temporale ", dt
 Print*, "Initial time:", time/years
 Print*, "Numero cicli:", int(n_cycle)
-Print*, "Coefficiente di diffusione ", D
 
 print*, 'Coefficienti della source? 0=non variabili, 1=variabili'
 read(*,*) coeff
@@ -198,7 +178,7 @@ do n=1, int(n_cycle)
   !!!Gradient of Zfe!!!
   do  j=2,jmax-1
     gradz_fe(j)=(z_fe(j)-z_fe(j-1))/(rr(j)-rr(j-1))  !! dZ/dr centered at "j" !!
-  enddo
+  end do
   gradz_fe(1)= 0. 
   gradz_fe(jmax)= 0.
 
@@ -228,7 +208,7 @@ do n=1, int(n_cycle)
    !!Computing the Fe mass
       M_fe(1) = 0.
       M_fe(j)=M_fe(j-1)+rho_fe(j-1)*vol(j)
-   enddo
+   end do
 !!!!!!!!!!Radial cyle finished!!!!!!!!
 !!(1) = (2), (jmax) = (jmax-1)!!
 z_fe(1)=z_fe(2)
@@ -254,5 +234,6 @@ close(30)
 close(40)
 !close(50)
 1005 format(6(1pe12.4))
+1006 format(3(1pe12.4))
 
 end 
